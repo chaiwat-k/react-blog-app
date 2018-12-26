ae31a90cd4dcad63914c742b22b422119c663657
Chaiwat Kanchanarat <Chaiwat_K@banpu.co.th>
2018-12-26 08:14:58 +0700

    Solve multiple GET /users/id calls with _.memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,12 +11,11 @@ export const fetchPosts = () => {
     };
 };
 
-export const fetchUser = (id) => {
-    return async (dispatch) => {
-        const response = await jsonplaceholder.get(`/users/${id}`);
-        dispatch({
-            type: 'FETCH_USER',
-            payload: response.data
-        });
-    };
-};
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonplaceholder.get(`/users/${id}`);
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    });
+});
