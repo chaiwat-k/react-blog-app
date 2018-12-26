cef70f7d47ed98b3220481f867ac3be72fa3aff0
Chaiwat Kanchanarat <Chaiwat_K@banpu.co.th>
2018-12-26 09:09:45 +0700

    Complete blog app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,8 @@ import postReducer from './postReducer';
 import userReducer from './userReducer';
 
 export default combineReducers({
+    // On the left is the state key to data in Redux store that is 
+    // going to be updated by the function on the right
     posts: postReducer,
     users: userReducer
 });
